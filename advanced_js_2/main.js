45742ee6b0c845b082f196a7fce962cb5e2d1813
amberward423 <amberward423@gmail.com>
2026-08-30 09:18:51 +0300

    Add restaurant company filter

---
+++ advanced_js_2/main.js
@@ -0,0 +1,53 @@
+import { restaurantRow , restaurantModal} from './components.js'; 
+
+import { fetchData } from './utils.js';
+
+import {baseUrl} from './variables.js';
+
+// your code here
+
+try {
+const restaurants = await fetchData(baseUrl);
+
+  const table = document.querySelector('table');
+  restaurants.sort((a, b) => a.name.localeCompare(b.name));
+  const select = document.querySelector('select')
+  console.log('table', table);
+
+  for (const restaurant of restaurants) {
+   
+    const row = restaurantRow(restaurant);
+        table.appendChild(row);
+ row.addEventListener('click', async () => {
+      const url = `${baseUrl}/daily/${restaurant._id}/en`; 
+      const data = await fetchData(url);
+
+      console.log(data);
+      document
+        .querySelectorAll('.highlight')
+        .forEach((element) => element.classList.remove('highlight'));
+
+    row.classList.add('highlight');
+      const dialog = document.querySelector('dialog');
+ dialog.innerHTML = modalHtml; 
+      dialog.show();
+    });
+  }
+select.addEventListener('change', async () => {
+     const selected = select.value;
+     const filtered = restaurants.filter(restaurant => restaurant.company === selected)
+table.querySelectorAll('tr:not(:first-child)').forEach((row) => {
+    table.removeChild(row);
+    });
+    filtered.forEach((restaurant) => {
+    const row = restaurantRow(restaurant);
+    table.appendChild(row);
+
+});
+    });
+
+
+} catch (error) {
+  alert('An error occurred:', error);
+  console.error('An error occurred:', error);
+}
